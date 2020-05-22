a6a6988e803681a749d5f691bc2f51a706c3e1d5
Marco Rodriguez <cap_marc@hotmail.com>
2020-05-21 22:50:00 -0500

    Se agrego el icono de el carrito y se creo el componente cartdropdown y se implemento la funcion de esconderlo y mostrarlo

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -3,21 +3,21 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, displayName, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="Logo" />
     </Link>
 
     <div className="options">
-      <span className="display-name">
-        {currentUser ? `Welcome ${currentUser.displayName}` : ''}
-      </span>
+      <span className="display-name">{`Welcome ${displayName}`}</span>
 
       <Link className="option" to="/shop">
         SHOP
@@ -34,12 +34,19 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({
+  user: { currentUser, displayName },
+  cart: { hidden },
+}) => ({
+  currentUser,
+  displayName,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
